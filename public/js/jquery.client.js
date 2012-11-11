49c6781bb66b3ba866b134bccb4ee19835295ce2
Sergii <x-sam@brainscode.com>
2012-11-11 03:59:12 +0200

    UI things for the main page

--- public/js/jquery.client.js
+++ public/js/jquery.client.js
@@ -30,6 +30,7 @@
 		if (service) {
 			$li.append($('<img class="service">').attr('src', service));
 		}
+/*
 		$$('#bubble ul').prepend($li);
 		$$('#bubble').scrollTop(98).stop().animate({
 			'scrollTop': '0'
@@ -41,6 +42,8 @@
 		setTimeout(function() {
 			socketIoClient.send('pong');
 		}, 1000);
+
+*/
 	});
 
 	socketIoClient.on('disconnect', function() {
@@ -52,6 +55,26 @@
 			maxZoom: 18,
 			attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
 		}).addTo(map);
+
+
+	function onLocationFound(e) {
+		var radius = e.accuracy / 2;
+
+		L.marker(e.latlng).addTo(map)
+			.bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+		L.circle(e.latlng, radius).addTo(map);
+	}
+
+	function onLocationError(e) {
+		alert(e.message);
+	}
+
+	map.on('locationfound', onLocationFound);
+	map.on('locationerror', onLocationError);
+
+	map.locate({setView: true, maxZoom: 14});
+
 
 
 
