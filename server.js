82d1c067c4b9877c05c0ed6fb534734ba8337ffb
Sergii <x-sam@brainscode.com>
2012-11-11 05:25:45 +0200

    f*cking redis

--- server.js
+++ server.js
@@ -27,18 +27,18 @@ var DummyHelper = require('./lib/dummy-helper');
 var RedisStore = require('connect-redis')(express);
 
 var redis = require('redis');
-var client = redis.createClient(6379, 'nodejitsudb8460898653.redis.irstack.com');
-client.auth('nodejitsudb8460898653.redis.irstack.com:f327cfe980c971946e80b8e975fbebb4', function (err) {
-	if (err) {
-		//throw err;
-		console.log(err);
-	}
-});
-console.log(redis);
-var sessionStore = new RedisStore(redis);
+var redisAuth = siteConf.redis.auth.split(':');
 
 var app = module.exports = express.createServer();
 app.listen(siteConf.port, null);
+
+var sessionStore = new RedisStore({
+                                    host: siteConf.redis.host,
+                                    port: siteConf.redis.port,
+                                    db: redisAuth[0],
+                                    pass: redisAuth[1]
+                                  });
+
 
 // Setup socket.io server
 var socketIo = new require('./lib/socket-io-server.js')(app, sessionStore);
